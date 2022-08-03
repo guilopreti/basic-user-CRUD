e8d603154f77ed65d88fa0dcdcf62f44589fd246
Guilherme <guilherme.llopreti@gmail.com>
2022-08-03 14:13:03 -0300

    feat: deploy configs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import cors from "cors";
 import usersRouter from "./routers/users.routes";
 import loginRouter from "./routers/login.routes";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 
-app.listen(3333);
+const port = 3333;
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`App running on port ${port}`);
+});
